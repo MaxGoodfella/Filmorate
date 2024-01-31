bc05bd44678a4b2ac3516f07ffedcdb3170dc19d
MaximGuseynov <MaxGoodfella@yandex.ru>
2024-01-31 16:05:18 +0500

    Sprint 9.4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/films")
 public class FilmController {
 
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final Logger log = LoggerFactory.getLogger(FilmController.class);
 
     private final List<Film> films = new ArrayList<>();
 
